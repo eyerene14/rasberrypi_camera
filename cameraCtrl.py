02fcda421c9b408ef15e311080b9dcc8c46f3c3b
eyerene <eyerene14@hotmail.com>
2022-11-09 14:17:23 -0800

    Not sure if I commited gitFormCameraCmds

--- cameraCtrl.py
+++ cameraCtrl.py
@@ -12,15 +12,15 @@ def activateCamera():
 def takePicture(new_picName='mynewpic'):
     # takes a still image
     # stores picture in home folder
-    'raspistill - o ' + new_picName + '.jpeg'
+    'raspistill -o ' + new_picName + '.jpeg'
 
 
 def createVideo(new_videoName='mynewvid', recording_duration=10000):
     # creates a video stream
     # duration of video conversion
     # 1000 = 1 sec; 10000 = 10 sec
-    os.system('raspivid - o ' + new_videoName +
-              '.h264 - t ' + str(recording_duration))
+    os.system('raspivid -o ' + new_videoName +
+              '.h264 -t ' + str(recording_duration))
 
 
 # createVideo('111022_830pm')
